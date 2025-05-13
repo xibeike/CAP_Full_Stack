937a42cda5237cac13456e8038550f2275ec8afa
beike xi <xibeike2024@163.com>
2025-05-13 02:26:37 +0000

    Full Stack Cap

---
+++ srv/src/main/java/customer/incident_management/Application.java
@@ -0,0 +1,13 @@
+package customer.incident_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
